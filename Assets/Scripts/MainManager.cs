5236d126f0034c55638b7dc11bbc67ebe9f8b249
Bacha12345 <bachaanwar26@gmail.com>
2026-02-01 12:28:56 +0100

    update2

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -74,4 +74,8 @@ public class MainManager : MonoBehaviour
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class MenuHandler : MonoBehaviour
 {
+    public TextMeshProUGUI Name;
+
+    public void NewName(TextMeshProUGUI name)
+    {
+        Manager.Instance.Name=name;
+    }
     public void StartNew()
     {
         SceneManager.LoadScene(1);
